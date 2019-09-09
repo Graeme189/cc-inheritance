9bf757e74583381d13a1f1e659933d29f301db15
lawrie-sm <alertrelic@gmail.com>
2019-09-09 13:14:45 +0100

    Initial

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+    private final double epsilon = 0.00001;
+
+    @Before
+    public void before() {
+       manager = new Manager("Ryan", "JG XX 01", 30000, "IT");
+    }
+
+    @Test
+    public void testName() {
+        assertEquals("Ryan", manager.getName());
+    }
+
+    @Test
+    public void testNiNumber() {
+        assertEquals("JG XX 01", manager.getNiNumber());
+    }
+
+    @Test
+    public void testSalary() {
+        assertEquals(30000, manager.getSalary(), epsilon);
+    }
+
+    @Test
+    public void testDeptName() {
+        assertEquals("IT", manager.getDeptName());
+    }
+}
